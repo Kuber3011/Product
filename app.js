29e0638d5c4137f8d240928d8c21c9e397487f40
devwebgiantstudio <63298361+devwebgiantstudio@users.noreply.github.com>
2020-11-27 00:05:48 +0530

    New Commit

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const PatientRoutes = require("./router/patient");
 app.use("/api/patient", PatientRoutes);
 
 //port 
-const PORT =  5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, function () {
     //  console.log(`Listening on ${port}`);
